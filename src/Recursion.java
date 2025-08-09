70c2881900258e55a48ec4d701285f68340542de
yuvarajgit-2505 <yuvaraj25525@gmail.com>
2025-08-09 13:19:50 +0530

    Recursion file added

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Recursion {
+    public  static  int findmin(int[] arr){
+        if(arr.length==1)
+            return arr[0];
+        return Math.max(arr[0],findmin(Arrays.copyOfRange(arr,1,arr.length)));
+    }
+
+    public static void main(String[] args) {
+        int[] arr={99,22,35,66};
+        System.out.println(findmin(arr));
+    }
+
 }
+
